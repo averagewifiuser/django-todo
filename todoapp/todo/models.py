747723725d71c47376c0403f70697778d68029a7
@joseph3241 <joseph3241@gmail.com>
2022-03-14 21:53:42 +0000

    setup the models

--- todoapp/todo/models.py
+++ todoapp/todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class TodoList(models.Model):
+    created = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=30, blank=False)
+    owner = models.ForeignKey('auth.User', related_name='todolists', on_delete=models.CASCADE)
+
+
+class Tasks(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    status = models.CharField(max_length=50, blank=False, default='pending')
+    description = models.TextField()
+    due = models.DateTimeField()
+    todo = models.ForeignKey(TodoList)
+
+    class Meta:
+        ordering = ['-created']
+
--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'todo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
